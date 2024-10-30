19d1680194b328e7526f1598a0874118523d757b
Zahida313 <zahidaasghar.k@gmail.com>
2024-10-30 17:41:12 +0500

    Add files via upload

---
+++ firstclass.py
@@ -0,0 +1,56 @@
+#Q no 1: calculate the sum, product, difference and average.
+#sum of numbers="2,4 and 6"
+sum1=2            #int
+sum2=4            #int
+sum3=6            #int
+add=(sum1+sum2+sum3)
+print("sum_result",add)
+
+#difference of numbers="3,5 and 10"
+difference_1=3           #int
+difference_2=5           #int
+difference_3=10          #int
+subtract=(difference_1-difference_2-difference_3)
+print("difference_result",subtract)
+
+#multiply_of_numbers="3,5 and10"
+product_1=3            #int
+product_2=5            #int
+product_3=10           #int
+multiply=(product_1*product_2*product_3)
+print("multiply_result",multiply)
+
+#average_of_numbers="2,6 and10"
+add1=2            #int
+add2=6            #int
+add3=10           #int
+average=(add1+add2+add3)/3
+print("average_result",average)
+
+#Q:no2 :  write python code calculate the area of the following:
+
+#area of triangle = "0.5*b*h"
+base=2          #int
+height=10       #int
+area=(0.5*base*height)
+print("triangle_area",area)
+
+#area of square ="s*s"
+side=4          #int
+area=(side*side)
+print("area of square",area)
+
+#area of rectangle="l*h"
+length=4          #int
+height=2          #int
+area=(length*height)
+print("area of rectangle",area)
+
+#area of circle ="pi*r*r"
+radius=10         #int
+pi=3.14           #float
+area=(pi*radius*radius)
+print("area of circle",area)
+
+
+
